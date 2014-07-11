fb92cde42095324c3c43e705c072b0b3fd5094bd
adriansoghoian <adriansoghoian@gmail.com>
2014-07-11 15:02:05 -0400

    Particles scrolling down now, changing colors. Moving onto Matrix-style animation now...

--- shapes/public/js/particle.js
+++ shapes/public/js/particle.js
@@ -11,7 +11,7 @@ function init() {
   camera.position.z = 1000;
 
   scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2( 0x000000, 0.002 );
+  scene.fog = new THREE.FogExp2( 0x000000, 0.0022 );
 
   geometry = new THREE.Geometry();
 
@@ -92,14 +92,22 @@ function render() {
 
   camera.lookAt(scene.position);
 
+  for ( i = 0; i < scene.children.length; i ++ ) {
+    var object = scene.children[ i ];
+    if ( object instanceof THREE.ParticleSystem ) {
+      object.rotation.x = time * ( i < 4 ? i - 0.5 : + ( i - 0.5 ) );
+    }
+  }
+
   for ( i = 0; i < materials.length; i ++ ) {
     color = parameters[i][0];
     h = ( 360 * ( color[0] + time ) % 360 ) / 360;
     materials[i].color.setHSL( h, color[1], color[2] );
   }
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
+
 
 function animate() {
   requestAnimationFrame(animate);
